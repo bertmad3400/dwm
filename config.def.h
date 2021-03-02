818d8a2ba41077dfa32662e5399f1f630ffacb53
bertmad3400 <skrivtilbertram@gmail.com>
2021-03-02 18:18:18 +0100

    Updated a few keybinds

--- config.def.h
+++ config.def.h
@@ -72,18 +72,20 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.v = scratchpadcmd } },
 	
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	
+	/* Not used as i dislike having multiple master windows. If used the d key will conflict with calling dmenu 
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	
+	*/	
+
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	
@@ -114,11 +116,11 @@ static Key keys[] = {
 	{ MODKEY,			XK_u,	   spawn,	   SHCMD("setxkbmap us")},
 	{ MODKEY|ShiftMask,		XK_u,	   spawn,	   SHCMD("setxkbmap dk")},
 	
-	
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+
+	{ MODKEY|ShiftMask,             XK_Tab,    zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,	                XK_q,      killclient,     {0} },
 	
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
