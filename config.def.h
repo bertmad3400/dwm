38617588872a819c68c175d651e37c9408e9a471
bertmad3400 <skrivtilbertram@gmail.com>
2021-03-08 10:03:00 +0100

    A little bit of cleanup and adjustments

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -75,20 +75,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.v = scratchpadcmd } },
-	
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	
+
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	
+
 	/* Not used as i dislike having multiple master windows. If used the d key will conflict with calling dmenu 
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	*/	
+	*/
 
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	
+
 	/* Stock keybinds for vanitygaps, not used
 	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
@@ -106,7 +106,7 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },*/
-	
+
 	/* Custom keybinds for vanitygaps */
 	{ MODKEY,                       XK_a,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,             XK_a,      defaultgaps,    {0} },
@@ -115,18 +115,18 @@ static Key keys[] = {
 
 	{ MODKEY,			XK_u,	   spawn,	   SHCMD("setxkbmap us")},
 	{ MODKEY|ShiftMask,		XK_u,	   spawn,	   SHCMD("setxkbmap dk")},
-	
 
-	{ MODKEY,             XK_space,  zoom,           {0} },
+
+	{ MODKEY,             		XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	
+
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	
+
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-	
+
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	
 	{ MODKEY,                       XK_Left,  focusmon,       {.i = -1 } },
@@ -143,11 +143,11 @@ static Key keys[] = {
         { MODKEY|ShiftMask,             XK_Down, tagcycle,       { .i = -1 } },
         { MODKEY,                       XK_Up,   cycle,          { .i = +1 } },
         { MODKEY|ShiftMask,             XK_Up,   tagcycle,       { .i = +1 } },
-	
+
 
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	
+
 	/* Program specific keybinds */
 	{ MODKEY|ShiftMask,		XK_k,        spawn,	   SHCMD("keepassxc")},
 	{ MODKEY|ShiftMask,		XK_w,        spawn,	   SHCMD("firefox")},
@@ -161,8 +161,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_KP_Prior,	spawn,	   SHCMD("slock")},
 	{ MODKEY|ShiftMask,		XK_KP_Left,	spawn,	   SHCMD("sudo reboot")},
 	{ MODKEY|ShiftMask,		XK_KP_Begin,	spawn,	   SHCMD("sudo shutdown now")},
-	{ MODKEY,			XK_KP_Subtract,	spawn,	   SHCMD("pulsemixer --change-volume -15")},
-	{ MODKEY,			XK_KP_Add,	spawn,	   SHCMD("pulsemixer --change-volume +15")},
+	{ MODKEY,			XK_KP_Subtract,	spawn,	   SHCMD("pulsemixer --change-volume -7")},
+	{ MODKEY,			XK_KP_Add,	spawn,	   SHCMD("pulsemixer --change-volume +7")},
 	{ MODKEY,			XK_equal,	spawn,	   SHCMD("pulsemixer --toggle-mute")},
 
 
@@ -197,4 +197,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
