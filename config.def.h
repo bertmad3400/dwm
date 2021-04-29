bd08ab4bb087ef87fab0aa939be881b14bf45925
bertmad3400 <skrivtilbertram@gmail.com>
2021-04-29 11:41:20 +0200

    Change colors
    Changed most of those in the bar to gruvbox and the border of unselected
    windows to the same color as the background of the terminal

--- config.def.h
+++ config.def.h
@@ -5,28 +5,26 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_red[]         = "#770000";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
+static const unsigned int borderpx	= 3;        /* border pixel of windows */
+static const unsigned int snap		= 32;       /* snap pixel */
+static const unsigned int gappih	= 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv	= 10;       /* vert inner gap between windows */
+static const unsigned int gappoh	= 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov	= 15;       /* vert outer gap between windows and screen edge */
+static       int smartgaps		= 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar		= 1;        /* 0 means no bar */
+static const int topbar			= 1;        /* 0 means bottom bar */
+static const int swallowfloating	= 0;        /* 1 means swallow floating windows by default */
+static const char *fonts[]		= { "monospace:size=10" };
+static const char dmenufont[]		= "monospace:size=10";
+static const char bg_col[]		= "#282828";
+static const char fg_col[]		= "#ebdbb2";
+static const char green_col[]		= "#98971a";
+static const char red_col[]		= "#fb4934";
+static const char *colors[][3]		= {
+	/*               fg		bg		border   */
+	[SchemeNorm] = { fg_col,	green_col,	bg_col},
+	[SchemeSel]  = { fg_col,	bg_col,		red_col},
 };
 
 /* tagging */
@@ -91,7 +89,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", bg_col, "-nf", fg_col, "-sb", green_col, "-sf", fg_col, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { TERMINAL, "-t", scratchpadname, "-g", "120x34", NULL };
