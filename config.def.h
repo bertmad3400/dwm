96be062de6078ccbe7c1bcdf6e6d6ab49517c9ee
bertmad3400 <skrivtilbertram@gmail.com>
2021-05-03 14:28:37 +0200

    Let dmenu run with standard colors

--- config.def.h
+++ config.def.h
@@ -93,7 +93,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", bg_col, "-nf", fg_col, "-sb", green_col, "-sf", fg_col, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { TERMINAL, "-t", scratchpadname, "-g", "120x34", NULL };
